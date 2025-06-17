0d6f46a7ee137e9531d235961ad941d700bc83fe
Disezmike <mikenatchapon@gmail.com>
2025-06-17 20:10:54 +0700

    chore: edit vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
+import {config} from 'dotenv';
+config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
+  define: {
+    "process.env": process.env
+  },
+  server: {
+    port: 3000,
+    allowedHosts: true
+  },
+  preview: {
+    port: 3000,
+    allowedHosts: true
+  },
+   build: {
+      chunkSizeWarningLimit: 1600
+   }
 });

f9c571b3b3928c1d8fa4ebcc7bb8225cd19ca368
Disezmike <mikenatchapon@gmail.com>
2025-06-18 00:42:19 +0700

    fix: process.env.PROD_URL -> process.env.PROD_HOST

--- src/client/function/auth.ts
+++ src/client/function/auth.ts
@@ -1,6 +1,6 @@
 import { TokenResponse } from '@react-oauth/google';
 import axios from 'axios';
-const API_URL = import.meta.env.PROD ? process.env.PROD_URL + "/api" : `http://localhost:${process.env.PORT}/api`;
+const API_URL = import.meta.env.PROD ? process.env.PROD_HOST + "/api" : `http://localhost:${process.env.PORT}/api`;
 
 export const useGoogleLogin = async (data: Omit<TokenResponse, "error" | "error_description" | "error_uri">) => {
     return axios.post(API_URL+"/login", {

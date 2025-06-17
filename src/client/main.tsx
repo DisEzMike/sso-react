081659d904be1687e31a4feb2698e722af4a409f
Disezmike <mikenatchapon@gmail.com>
2025-06-17 16:45:08 +0700

    feat: set route for home page

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -4,9 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { BrowserRouter, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route path="/signin" element={<App />} />
+    </BrowserRouter>
   </React.StrictMode>,
 );

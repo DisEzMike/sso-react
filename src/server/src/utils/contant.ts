a81425b177e58a399a390cdb09be9ba606cff20a
Disezmike <mikenatchapon@gmail.com>
2025-06-17 22:46:31 +0700

    fix: enable dotenv config

--- src/server/src/utils/contant.ts
+++ src/server/src/utils/contant.ts
@@ -1,3 +1,7 @@
+import { config } from "dotenv";
+
+config();
+
 export const DB_HOST = process.env.DB_HOST;
 export const DB_PORT = process.env.DB_PORT
 export const DB_DATABASE = process.env.DB_DATABASE

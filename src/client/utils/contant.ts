ae6495c640d6c1bdb9edd21b8e43287e9d7cf26b
Disezmike <mikenatchapon@gmail.com>
2025-06-18 23:00:00 +0700

    fix: Host

--- src/client/utils/contant.ts
+++ src/client/utils/contant.ts
@@ -1,4 +1,4 @@
-// export const API_HOST = import.meta.env.PROD ? process.env.PROD_HOST : "http://localhost:" + process.env.PORT;
-export const HOST = import.meta.env.PROD ? process.env.PROD_HOST : process.env.PROD_HOST;
+export const HOST = import.meta.env.PROD ? process.env.PROD_HOST : "http://localhost:" + process.env.PORT;
+// export const HOST = import.meta.env.PROD ? process.env.PROD_HOST : process.env.PROD_HOST;
 export const AUTH_URL = HOST + "/oauth";
 export const API_URL = HOST + "/api";

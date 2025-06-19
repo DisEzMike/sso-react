d47d1a3d026bb88d2c07a901efce4fa87f046582
Disezmike <mikenatchapon@gmail.com>
2025-06-19 12:06:12 +0700

    chore: add ProtoLoginType

--- src/server/src/utils/type.ts
+++ src/server/src/utils/type.ts
@@ -2,22 +2,29 @@ import { TokenResponse } from "@react-oauth/google";
 import { Request } from "express";
 import { IUser } from "../database/model/User.ts";
 
-interface localLogin {
-    type: 'local';
+interface ProtoLoginType {
     data: {
+        client_id: string;
+        redirect_uri: string;
+        state: string;
+    }
+}
+interface localLogin extends ProtoLoginType {
+    type: 'local';
+    data: ProtoLoginType['data'] & {
         email: string;
         password: string;
     }
 }
 
-interface googleLogin {
+interface googleLogin extends ProtoLoginType {
     type: "google";
-    data: Omit<TokenResponse, "error" | "error_description" | "error_uri"> & {client_id: string, redirect_uri: string, state: string}
+    data: ProtoLoginType['data'] & Omit<TokenResponse, "error" | "error_description" | "error_uri">
 }
 
 interface lineLogin {
     type: "line";
-    data: any
+    data: ProtoLoginType['data'] & any
 }
 export type loginType = localLogin | googleLogin | lineLogin;
 

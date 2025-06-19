fd5c85098178748196f435025458268d6e1553cc
Disezmike <mikenatchapon@gmail.com>
2025-06-19 10:43:15 +0700

    fix: error not get property

--- src/server/src/controllers/admin.controller.ts
+++ src/server/src/controllers/admin.controller.ts
@@ -3,19 +3,19 @@ import { IRequest } from "../utils/type.ts";
 import { generateClientCredentials } from "../utils/cryptoUtils.ts";
 
 export const createClient: any = (req: IRequest, res: Response) => {
-  const { redirectUris } = req.body;
+  try {
+  const {redirectUris} = req.body;
   if (!redirectUris || !Array.isArray(redirectUris) || redirectUris.length === 0) {
     return res.status(400).json({ error: 'redirectUris (array) is required' });
   }
-
+  
   const { clientId, clientSecret } = generateClientCredentials();
-
-  try {
-    // const newClient = await Client.create({
-    //   clientId,
-    //   clientSecret,
-    //   redirectUris,
-    // });
+  
+    const newClient = await Client.create({
+      clientId,
+      clientSecret,
+      redirectUris,
+    });
 
     res.json({
       clientId: clientId,
@@ -23,6 +23,7 @@ export const createClient: any = (req: IRequest, res: Response) => {
       redirectUris: redirectUris,
     });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to create client' });
+    const message = error instanceof Error ? error.message : 'Failed to create client';
+    res.status(500).json({message});
   }
 };

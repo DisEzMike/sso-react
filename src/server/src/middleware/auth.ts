4a6aa88be288347475e0910fcdd6022db961e309
Disezmike <mikenatchapon@gmail.com>
2025-06-19 10:29:51 +0700

    feat: add checkAdmin middleware

--- src/server/src/middleware/auth.ts
+++ src/server/src/middleware/auth.ts
@@ -26,24 +26,25 @@ export const authMiddleware:any = async (req: IRequest, res: Response, next: Nex
 }
 
 
-// export const checkAdmin:any = async (req: IRequest, res: Response, next: NextFunction) => {
-//     try {
-//         //code
-//         const token = req.headers["x-access-token"] as string
+export const checkAdmin:any = async (req: IRequest, res: Response, next: NextFunction) => {
+    try {
+        //code
+        const token = req.headers["x-access-token"] as string
 
-//         if (!token) {
-//             return res.status(401).send('No token');
-//         }
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET!) as any;
+        if (!token) {
+            return res.status(401).send('No token');
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as any;
+        const {payload} = decoded;
 
-//         const user = await User.findOne({userId: decoded.user.userId});
-//         req.user = user;
+        const user = await User.findByIdAndUpdate<IUser>(payload.user._id, {new: true});
+        req.user = user!;
         
-//         if (req.user.role == 'admin') next();
-//         else res.send("You are not allow to get this route").status(403);
-//     } catch (err) {
-//         // err
-//         console.error(err)
-//         res.send('Token Invalid').status(500)
-//     }
-// } 
+        if (req.user.role == 0) next();
+        else res.send("You are not allow to get this route").status(403);
+    } catch (err) {
+        // err
+        console.error(err)
+        res.send('Token Invalid').status(500)
+    }
+} 

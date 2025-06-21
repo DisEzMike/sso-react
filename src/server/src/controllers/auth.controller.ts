083e1df27b968ed45152e81bf7958fa2fc021127
Disezmike <mikenatchapon@gmail.com>
2025-06-21 13:33:28 +0700

    feat: add get method in authorize

--- src/server/src/controllers/auth.controller.ts
+++ src/server/src/controllers/auth.controller.ts
@@ -16,55 +16,41 @@ import { signToken, verifyToken } from "../utils/jwt.ts";
 
 export const authorize: any = async (req: Request, res: Response) => {
 
-    if (!req.body) res.status(403).json({status: 403, message: "body not provide."});
-    else {
-        const {body} = req;
-    
-        const {type, data} = body as loginType;
+    if (req.method == 'GET') {
+        let {scope, response_type, client_id, redirect_uri, state} = req.query;
 
-        const client = await Client.findOne({ client_id: data.client_id });
-            if (!client || !client.redirectUris.includes(data.redirect_uri)) {
-            return res.status(400).json({status: 400, message: 'Invalid client or redirect URI'});
+        if (!response_type || response_type !== 'code') return res.status(400).send('Missing or unsupported response_type');
+        
+        if (!scope || !(scope as string).includes('openid')) return res.status(400).send('Missing "openid" scope');
+
+        if (!redirect_uri) return res.send('Missing "redirect_uri"');
+
+        const client = await Client.findOne({ client_id });
+        if (!client || !client.redirectUris.includes(redirect_uri as string)) {
+            return res.status(400).send('Invalid client or redirect URI');
         }
-    
-        if (type == "local") {
-            const {username, password} = data;
-            const user = await User.findOneAndUpdate({$or: [{username}, {email: username}]}, {new:true});
-            if (!user) return res.status(401).json({status: 401, message: "user not found"});
 
-            const validPassword = await bcrypt.compare(password, user.password);
-            if (!validPassword) return res.status(401).json({status: 401, message: "password not match"});
+        const login_url = `/?scope=${scope}&response_type=${response_type}&client_id=${client_id}&redirect_uri=${redirect_uri}&state=${state}`;
+        return res.send(`<script>window.location.href = '${login_url}'</script>`)
+    } else if (req.method == 'POST') {
+        if (!req.body) res.status(403).json({status: 403, message: "body not provide."});
+        else {
+            const {body} = req;
+        
+            const {type, data} = body as loginType;
 
-            const authCode = await getAuthCode({
-                client_id: data.client_id,
-                user_id: user!._id,
-                redirect_uri: data.redirect_uri,
-            })
+            const client = await Client.findOne({ client_id: data.client_id });
+                if (!client || !client.redirectUris.includes(data.redirect_uri)) {
+                return res.status(400).json({status: 400, message: 'Invalid client or redirect URI'});
+            }
+        
+            if (type == "local") {
+                const {username, password} = data;
+                const user = await User.findOneAndUpdate({$or: [{username}, {email: username}]}, {new:true});
+                if (!user) return res.status(401).json({status: 401, message: "user not found"});
 
-            res.json({redirect_url: `${data.redirect_uri}?code=${authCode.code}&state=${data.state}`});
-        } else if (type == "google") {
-            try {    
-                const response = await axios.get("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", {
-                    headers: {
-                        Authorization: `Bearer ${data.access_token}`
-                    }
-                });
-                const profile = response.data as googleProfile
-                const authData = {type, profile};
-                const providerUser = await ProviderUser.findOneAndUpdate({providerId: authData.profile.id});
-                let user;
-                if (providerUser) {
-                    user = await User.findByIdAndUpdate(providerUser.userId, {new: true}); 
-                } else {
-                    const new_providerUser = new ProviderUser({
-                        type: authData.type,
-                        providerId: authData.profile.id,
-                        data: authData.profile
-                    })
-                    await new_providerUser.save();
-
-                    user = await User.findByIdAndUpdate(new_providerUser.userId, {new: true}); 
-                }
+                const validPassword = await bcrypt.compare(password, user.password);
+                if (!validPassword) return res.status(401).json({status: 401, message: "password not match"});
 
                 const authCode = await getAuthCode({
                     client_id: data.client_id,
@@ -73,15 +59,49 @@ export const authorize: any = async (req: Request, res: Response) => {
                 })
 
                 res.json({redirect_url: `${data.redirect_uri}?code=${authCode.code}&state=${data.state}`});
-            } catch (error) {
-                console.log(error);
-            }
+            } else if (type == "google") {
+                try {    
+                    const response = await axios.get("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", {
+                        headers: {
+                            Authorization: `Bearer ${data.access_token}`
+                        }
+                    });
+                    const profile = response.data as googleProfile
+                    const authData = {type, profile};
+                    const providerUser = await ProviderUser.findOneAndUpdate({providerId: authData.profile.id});
+                    let user;
+                    if (providerUser) {
+                        user = await User.findByIdAndUpdate(providerUser.userId, {new: true}); 
+                    } else {
+                        const new_providerUser = new ProviderUser({
+                            type: authData.type,
+                            providerId: authData.profile.id,
+                            data: authData.profile
+                        })
+                        await new_providerUser.save();
 
-        } else if (type == "line") {
-            res.status(404).json({status: 404, message: "Local login is not active"});
-        } else {
-            res.status(404).json({status: 403, message: "type is not provide"});
+                        user = await User.findByIdAndUpdate(new_providerUser.userId, {new: true}); 
+                    }
+
+                    const authCode = await getAuthCode({
+                        client_id: data.client_id,
+                        user_id: user!._id,
+                        redirect_uri: data.redirect_uri,
+                    })
+
+                    res.json({redirect_url: `${data.redirect_uri}?code=${authCode.code}&state=${data.state}`});
+                } catch (error) {
+                    console.log(error);
+                }
+
+            } else if (type == "line") {
+                res.status(404).json({status: 404, message: "Local login is not active"});
+            } else {
+                res.status(404).json({status: 403, message: "type is not provide"});
+            }
         }
+    } else {
+        return res.status(405).json({status: 405, message: 'Method not allowed'});
     }
 }
 

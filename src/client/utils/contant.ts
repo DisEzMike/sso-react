58f610cb3d0f2d5c6ba8befbbc9412cae8d316b9
Disezmike <mikenatchapon@gmail.com>
2025-06-21 14:38:54 +0700

    chore: oauth -> auth

--- src/client/utils/contant.ts
+++ src/client/utils/contant.ts
@@ -1,5 +1,5 @@
 export const HOST = import.meta.env.PROD ? process.env.PROD_HOST : process.env.DEV_HOST;
-export const AUTH_URL = HOST + "/oauth";
+export const AUTH_URL = HOST + "/auth";
 export const API_URL = HOST + "/api";
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
